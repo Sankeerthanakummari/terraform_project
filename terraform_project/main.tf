d7a86b5bf5e2f7f41930f0a8225fa3e6345e604a
Sankeerthanakummari <155805855+Sankeerthanakummari@users.noreply.github.com>
2026-08-27 00:25:15 +0530

    updated resource block

--- terraform_project/main.tf
+++ terraform_project/main.tf
@@ -13,10 +13,12 @@ provider "aws" {
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
+  ami           = "ami-0468283b4d00417cf"
   instance_type = "t4g.small"
 
   tags = {
     Name = "Terraform_Demo"
   }
 }
+
+
