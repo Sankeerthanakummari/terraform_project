f048cd74d9cc8eee42cf9c46a5b34ff563d08aae
Ubuntu <ubuntu@ip-172-31-10-2.ap-south-1.compute.internal>
2026-08-27 18:12:36 +0000

    updated s3 bucket

--- terraform_project/backend.tf
+++ terraform_project/backend.tf
@@ -1,10 +1,10 @@
 terraform {
   backend "s3" {
-    bucket         = "my-terraform-state-bucket"   # Replace with your S3 bucket name
-    key            = "ec2/terraform.tfstate"       # Path inside the bucket
-    region         = "us-east-1"                   # Region of the S3 bucket
-    dynamodb_table = "terraform-locks"             # DynamoDB table for state locking
-    encrypt        = true                          # Encrypt state at rest
+    bucket         = "sankeerthana-cli-bucket"
+    key            = "ec2/terraform.tfstate"
+    region         = "ap-south-1"   # or your bucket’s region
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
   }
 }
 
